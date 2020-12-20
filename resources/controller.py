acc9debc3ffb1125cdb78adf4689f1cfb4427656
Jesse <jesse.smedberg@outlook.com>
2020-12-21 00:43:44 +0200

    Update controller.py

--- resources/controller.py
+++ resources/controller.py
@@ -3,20 +3,38 @@ from flask_restful import Resource
 import json
 from http import HTTPStatus
 
+from models.controllerValidator import controllerValidator
+
 class controllerResource(Resource):
 
 
     # Handles incoming controller data.
     # TODO: Add sender validation. Tokens, users etc.
     def post(self):
-        data = request.data
-        json_object = json.loads(data)
-        json_formatted = json.dumps(json_object, indent=2)
+        data = request.get_json()
 
-        print(json_formatted)
+        identifier = data.get('identifier')
+        print(data)
 
-        return HTTPStatus.CREATED
+        if controllerValidator.get_by_identifier(identifier):
+            return HTTPStatus.UNAUTHORIZED
 
+        controllerValidator.save()
+
+        return data, HTTPStatus.OK
+
+"""
 class controllerRegisterResource(Resource):
 
-    def
+    def post(self):
+        data = request.get_json()
+
+        id = data.get('id')
+        identifier = data.get('identifier')
+
+        if identifier not cV.get_by_id(identifier):
+            return HTTPStatus.UNAUTHORIZED
+
+
+        return HTTPStatus.OK
+"""
