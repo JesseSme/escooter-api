cf7a4d42d79a2547c67bf296024248be964c21b7
Jesse <jesse.smedberg@outlook.com>
2020-12-20 11:39:16 +0200

    Added keys for auth

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-from secret import Secret
+from secret import MONGODB_PASS, JWT_KEY, JWT_ERROR_KEY
 
 class Config:
 
-    MONGO_URI = "mongodb+srv://IAmRoot:{}@iot.fnq30.mongodb.net/tracking?retryWrites=true&w=majority".format(Secret.MONGODB_PASS)
+    MONGO_URI = "mongodb+srv://IAmRoot:{}@iot.fnq30.mongodb.net/tracking?retryWrites=true&w=majority".format(MONGODB_PASS)
+    SECRET_KEY = JWT_KEY
+    JWT_ERROR_MESSAGE_KEY = JWT_ERROR_KEY
