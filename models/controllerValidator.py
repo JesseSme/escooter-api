6c6f9866e8d062292504869ce2c533ce9e64c995
Jesse <jesse.smedberg@outlook.com>
2020-12-21 02:12:40 +0200

    Update controllerValidator.py

--- models/controllerValidator.py
+++ models/controllerValidator.py
@@ -1,10 +1,25 @@
 from extensions import mongo
+import json
+from collections import OrderedDict
+from flask import jsonify
 
 class controllerValidator():
 
     @classmethod
+    def set_validator(self):
+        validator = {}
+        with open('validators\controllerValidator.json') as validator_file:
+            validator = json.load(validator_file)
+        od = OrderedDict(validator)
+        mongo.db.command(od)
+
     def get_by_identifier(self, identifier):
-        return mongo.db.identifications.find_one({"identifier": identifier})
+        print(identifier)
+        #mongo.db.identifications.create_index({identifier: "text"})
+        gotid = mongo.db.identifications.find_one({"$text": {"$search": "\"{}\"".format(identifier)}})
+        if gotid["identifier"] == identifier:
+            return True
+        return False
 
 
     def save(self, data):
