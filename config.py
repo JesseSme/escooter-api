e6d6b5bb5a65ce766e415d3ac7a07b4a7bdfeb74
Jesse <jesse.smedberg@outlook.com>
2021-01-04 01:39:41 +0200

    Updated configs

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ class Config:
 
     MONGODB_SETTINGS = {
         "db": "tracking",
-        "host": "mongodb+srv://IAmRoot:{}@iot.fnq30.mongodb.net/tracking?retryWrites=true&w=majority".format(MONGODB_PASS)
+        "host": "mongodb+srv://IAmRoot:{}@iot.fnq30.mongodb.net/tracking?retryWrites=true&w=majority".format(MONGODB_PASS),
+        "connectTimeoutMS": 30000,
+        "socketTimeoutMS": None,
+        "socketKeepAlive": True,
+        "connect": False,
+        "maxPoolsize": 1
     }
     # MONGO_URI =
     SECRET_KEY = JWT_KEY
