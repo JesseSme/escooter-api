d0c166ef4e4a8e6c517749903641ec66299c7fd7
Jesse <jesse.smedberg@outlook.com>
2021-01-14 16:09:42 +0200

    Added some websocket stuff

--- resource/socket.py
+++ resource/socket.py
@@ -11,18 +11,34 @@ class WebSocketResource(Resource):
     def __init__(self):
         pass
 
+    @staticmethod
+    def inc_receive():
+        session["receive_count"] = session.get("receive_count", 0) + 1
+
+
     @socketio.on("hello", namespace="/")
     def hello_event(message):
-        session["receive_count"] = session.get("receive_count", 0) + 1
+        WebSocketResource.inc_receive()
         emit("response", {"message": message["data"] , "count": session["receive_count"]})
 
 
     @socketio.on("scooter_info", namespace="/")
     def scooter_info_event(message):
-
-        session["receive_count"] = session.get("receive_count", 0) + 1
+        WebSocketResource.inc_receive()
         scooter_data = Controller.objects.order_by("-updated_at").first()
         scooter_data = scooter_data.to_json()
         scooter_json = json.loads(scooter_data)
         scooter_json.pop("_id")
         emit("response", {"message": json.dumps(scooter_json), "count": session["receive_count"]})
+
+
+    @socketio.on("power_signal", namespace="/")
+    def power_signal_event(message):
+        session["receive_count"] = session.get("receive_count", 0) + 1
+        #if state == "on":
+        #Send turn on to controller
+        #Opposite on off
+        print(message["state"])
+        print(message["pass"])
+
+        emit("response", {"message": "Power signal sent.", "count": session["receive_count"]})
